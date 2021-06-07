ca402c1ed847975b444350b10007f800fcd9835f
Stevy <superstevy@yahoo.com>
2021-06-07 17:19:04 -0400

    small change

--- backend/backend/todolistapp/views.py
+++ backend/backend/todolistapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions
 from backend.todolistapp.serializers import UserSerializer, GroupSerializer
 
 
