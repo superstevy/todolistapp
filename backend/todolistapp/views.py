ccb9d1451188de769ed336d34b10425ba202eb05
Stevy <superstevy@yahoo.com>
2021-06-10 20:39:52 -0400

    fixes

--- backend/todolistapp/views.py
+++ backend/todolistapp/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
 from backend.todolistapp.serializers import UserSerializer, GroupSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
