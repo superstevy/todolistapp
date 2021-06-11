e008c4326baabc6b69550686efc83431fb84df13
Stevy <superstevy@yahoo.com>
2021-06-11 17:04:14 -0400

    changes in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from backend.todolistapp import views
+# from backend.todolistapp import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
